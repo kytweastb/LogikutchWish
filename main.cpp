9fe48cd53937400e9fda4fe97417fb6c40bf5fd4
Woody <cyclaus73@gmail.com>
2025-02-16 18:50:28 +0100

    Ajout triangle

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
+#include <algorithm>
+
+
 int main()
 {
     // create the window
     sf::RenderWindow window(sf::VideoMode({1200, 800}), "My window");
-
+    sf::ConvexShape shape;
+    shape.setFillColor(sf::Color::White);
+    shape.setPointCount(3);
+    shape.setPoint(0, sf::Vector2f(550, 300));
+    shape.setPoint(1, sf::Vector2f(550, 500));
+    shape.setPoint(2, sf::Vector2f(650, 400));
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -19,6 +27,7 @@ int main()
 
         // clear the window with black color
         window.clear(sf::Color(50,50,50));
+        window.draw(shape);
 
         // draw everything here...
         // window.draw(...);
